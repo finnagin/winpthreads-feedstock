38b39ad3550c697cdeb5a0bf1d6df67d1875dff9
Finn Womack <flan313@gmail.com>
2026-03-17 01:45:33 -0700

    update folder name in libwinpthread script

--- recipe/install_libwinpthread.bat
+++ recipe/install_libwinpthread.bat
@@ -3,5 +3,5 @@ if %target_platform%=="win-64" (
   xcopy %SRC_DIR%\binary-libwinpthread\ucrt64\bin\* %LIBRARY_BIN%\ /s /e /y
 )
 if %target_platform%=="win-arm64" (
-  xcopy %SRC_DIR%\binary-libwinpthread\clangarm64\bin\* %LIBRARY_BIN%\ /s /e /y
+  xcopy %SRC_DIR%\binary-libwinpthread-arm64\clangarm64\bin\* %LIBRARY_BIN%\ /s /e /y
 )
