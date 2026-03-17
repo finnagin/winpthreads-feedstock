26fe5502edd3eb1a1ef8184d88a598786503b423
Finn Womack <flan313@gmail.com>
2026-03-17 03:24:34 -0700

    add quotes to liwinpthreads install script

--- recipe/install_libwinpthread.bat
+++ recipe/install_libwinpthread.bat
@@ -1,7 +1,7 @@
 mkdir %LIBRARY_BIN%
-if %target_platform%=="win-64" (
+if "%target_platform%"=="win-64" (
   xcopy %SRC_DIR%\binary-libwinpthread\ucrt64\bin\* %LIBRARY_BIN%\ /s /e /y
 )
-if %target_platform%=="win-arm64" (
+if "%target_platform%"=="win-arm64" (
   xcopy %SRC_DIR%\binary-libwinpthread-arm64\clangarm64\bin\* %LIBRARY_BIN%\ /s /e /y
 )
