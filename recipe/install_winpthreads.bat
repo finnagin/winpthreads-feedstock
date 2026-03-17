263540c20db0e36050e74bc08a0e4d0326663b9f
Finn Womack <flan313@gmail.com>
2026-03-17 03:23:57 -0700

    add quotes

--- recipe/install_winpthreads.bat
+++ recipe/install_winpthreads.bat
@@ -1,11 +1,11 @@
 @echo on
 
 mkdir %LIBRARY_BIN%
-if %target_platform%=="win-64" (
+if "%target_platform%"=="win-64" (
   xcopy %SRC_DIR%\binary-winpthreads\ucrt64\lib\* %LIBRARY_LIB%\ /s /e /y
   xcopy %SRC_DIR%\binary-winpthreads\ucrt64\include\* %LIBRARY_INC%\ /s /e /y
 )
-if %target_platform%=="win-arm64" (
+if "%target_platform%"=="win-arm64" (
   xcopy %SRC_DIR%\binary-winpthreads-arm64\clangarm64\lib\* %LIBRARY_LIB%\ /s /e /y
   xcopy %SRC_DIR%\binary-winpthreads-arm64\clangarm64\include\* %LIBRARY_INC%\ /s /e /y
 )
@@ -14,10 +14,10 @@ echo LIBRARY libwinpthread-1 > libwinpthread-1.def
 echo EXPORTS >> libwinpthread-1.def
 for /f "skip=19 tokens=4" %%A in ('dumpbin /exports %LIBRARY_BIN%\libwinpthread-1.dll') do echo %%A >> libwinpthread-1.def
 
-if %target_platform%=="win-64" (
+if "%target_platform%"=="win-64" (
   lib /def:libwinpthread-1.def /out:pthread.lib /machine:x64
 )
-if %target_platform%=="win-arm64" (
+if "%target_platform%"=="win-arm64" (
   lib /def:libwinpthread-1.def /out:pthread.lib /machine:arm64
 )
 copy pthread.lib %LIBRARY_LIB%
